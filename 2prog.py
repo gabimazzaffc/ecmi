571b84bc05d82fe7fe2a3fad1d85e1fb58415ae1
gabimazzaffc <134002012+gabimazzaffc@users.noreply.github.com>
2023-05-23 08:16:23 -0300

    2prog.py

---
+++ 2prog.py
@@ -0,0 +1,16 @@
+import streamlit as st
+import panda as pd
+import numpy as np
+
+st.title('Teste ECMI 2')
+
+st.write("Tabela")
+
+dataframe = pd.DataFrame({
+  'Name': ['Josir', 'Gabi', 'Lua', 'Manu']
+  'Salário': [10, 20, 30, 40]
+})
+dataframe.style.highlight_max(axis=0)
+
+st.write(dataframe)
+
