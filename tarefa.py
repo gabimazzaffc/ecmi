18032ccf689513ec0ecc0369f75454394900a9ff
gabimazzaffc <134002012+gabimazzaffc@users.noreply.github.com>
2023-05-29 13:01:41 -0300

    Update tarefa.py

--- tarefa.py
+++ tarefa.py
@@ -1,7 +1,11 @@
 import streamlit as st
-
+import pandas as pd
 st.title('Trabalho de programação')
+st.caption('Gabriela Mazza')
 
-arquivo = open('marcas-carros.csv')
-for linha in arquivo: 
-   st.write(linha)
+df = pd.read_csv('marcas-carros.csv')
+st.dataframe(df)
+
+#arquivo = open('marcas-carros.csv')
+#for linha in arquivo: 
+#   st.write(linha)
