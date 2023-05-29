d4693ab06b1e9b38a7bf7f0e02beb237f103ec52
gabimazzaffc <134002012+gabimazzaffc@users.noreply.github.com>
2023-05-29 13:03:24 -0300

    Update tarefa.py

--- tarefa.py
+++ tarefa.py
@@ -3,7 +3,7 @@ import pandas as pd
 st.title('Trabalho de programação')
 st.caption('Gabriela Mazza')
 
-df = pd.read_csv('marcas-carros.csv')
+df = pd.read_csv('marcas-carros.csv', sep=';')
 st.dataframe(df)
 
 #arquivo = open('marcas-carros.csv')
