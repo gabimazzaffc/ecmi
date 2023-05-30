879c58211b4e201b410524c8950b50c2ec5ca59f
gabimazzaffc <134002012+gabimazzaffc@users.noreply.github.com>
2023-05-30 15:07:22 -0300

    Update tarefa2.py

--- tarefa2.py
+++ tarefa2.py
@@ -1,17 +1,14 @@
 import streamlit as st
 import pandas as pd
+import numpy as np
 st.title('Trabalho csv2')
 st.caption('Gabriela Mazza')
 
 df = pd.read_csv('happiness.csv', sep=',')
 st.dataframe(df)
 
-import streamlit as st
-import pandas as pd
-import numpy as np
-
 chart_data = pd.DataFrame(
-    np.happinnes(155, 3),
+    np.random.randn(155, 3),
     columns=['Healthy life expectancy', 'Generosity', 'Social support'])
 
 st.line_chart(happinnes)
