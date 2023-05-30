5daff53ec7c208da4adb4b1dca1a7e9be03c53c4
gabimazzaffc <134002012+gabimazzaffc@users.noreply.github.com>
2023-05-30 15:09:10 -0300

    Update tarefa2.py

--- tarefa2.py
+++ tarefa2.py
@@ -8,7 +8,7 @@ df = pd.read_csv('happiness.csv', sep=',')
 st.dataframe(df)
 
 chart_data = pd.DataFrame(
-    np.random.randn(155, 3),
+    np.random.randn(5, 3),
     columns=['Healthy life expectancy', 'Generosity', 'Social support'])
 
 st.line_chart(chart_data)
