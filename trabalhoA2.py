eeb29ed44426b9265b3849e0a4a93c72cd1f2c97
gabimazzaffc <134002012+gabimazzaffc@users.noreply.github.com>
2023-05-30 09:28:40 -0300

    Update trabalhoA2.py

--- trabalhoA2.py
+++ trabalhoA2.py
@@ -3,6 +3,9 @@ import pandas as pd
 df = pd.read_parquet('dados_gruposoma.parquet')
 df
 
+
+df['Post Created Date'] = pd.to_datetime(df['Post Created Date'])
+df['Post Created'] = pd.to_datetime(df['Post Created'].str[:-4], format='%Y-%m-%d %H:%M:%S').dt.tz_localize('America/Sao_Paulo')
 df['Mês'] = df['Post Created Date'].dt.month
 df['Ano'] = df['Post Created Date'].dt.year
 media_interactions_mes_ano = df.groupby(['Account', 'Mês','Ano'])['Total Interactions'].mean().reset_index().sort_values(by=['Account', 'Ano', 'Mês'])
