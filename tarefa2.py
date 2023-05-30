d4ad9ba8e50212ade0682a31e3088197dc28ec92
gabimazzaffc <134002012+gabimazzaffc@users.noreply.github.com>
2023-05-30 14:57:27 -0300

    Update tarefa2.py

--- tarefa2.py
+++ tarefa2.py
@@ -5,3 +5,9 @@ st.caption('Gabriela Mazza')
 
 df = pd.read_csv('happiness.csv', sep=',')
 st.dataframe(df)
+
+import streamlit as st
+import pandas as pd
+import numpy as np
+
+st.line_chart(data= happiness, x=Healthy life expectancy, y=Generosity, use_container_width=True)
