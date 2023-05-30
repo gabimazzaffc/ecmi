30201b6916307514a7828eeba4c0fa8cb6d34584
gabimazzaffc <134002012+gabimazzaffc@users.noreply.github.com>
2023-05-30 15:04:25 -0300

    Update tarefa2.py

--- tarefa2.py
+++ tarefa2.py
@@ -5,15 +5,14 @@ st.caption('Gabriela Mazza')
 
 df = pd.read_csv('happiness.csv', sep=',')
 st.dataframe(df)
-df
 
 import streamlit as st
 import pandas as pd
 import numpy as np
 
 chart_data = pd.DataFrame(
-    np.happinnes(20, 3),
-    columns=['Healthy life expectancy', 'Generosity', 'Social suport'])
+    np.happinnes(155, 3),
+    columns=['Healthy life expectancy', 'Generosity', 'Social support'])
 
 st.line_chart(happinnes)
 
