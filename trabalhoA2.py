ae45e98538fc9f76610d09cc968029be000a589b
gabimazzaffc <134002012+gabimazzaffc@users.noreply.github.com>
2023-05-30 09:06:37 -0300

    Update trabalhoA2.py

--- trabalhoA2.py
+++ trabalhoA2.py
@@ -4,11 +4,11 @@ df = pd.read_parquet('dados_gruposoma.parquet')
 df
 
 df['Mês'] = df['Post Created Date'].dt.month
-# df['Ano'] = df['Post Created Date'].dt.year
-# media_interactions_mes_ano = df.groupby(['Account', 'Mês','Ano'])['Total Interactions'].mean().reset_index().sort_values(by=['Account', 'Ano', 'Mês'])
-# media_interactions_mes_ano['Interactions Growth'] = media_interactions_mes_ano.groupby('Account')['Total Interactions'].transform(lambda x: x.pct_change())
-# media_interactions_ano = df.groupby(['Account', 'Ano'])['Total Interactions'].mean().reset_index().sort_values(by=['Account', 'Ano'])
-# media_interactions_ano['Interactions Growth'] = media_interactions_ano.groupby('Account')['Total Interactions'].pct_change()
+df['Ano'] = df['Post Created Date'].dt.year
+media_interactions_mes_ano = df.groupby(['Account', 'Mês','Ano'])['Total Interactions'].mean().reset_index().sort_values(by=['Account', 'Ano', 'Mês'])
+media_interactions_mes_ano['Interactions Growth'] = media_interactions_mes_ano.groupby('Account')['Total Interactions'].transform(lambda x: x.pct_change())
+media_interactions_ano = df.groupby(['Account', 'Ano'])['Total Interactions'].mean().reset_index().sort_values(by=['Account', 'Ano'])
+media_interactions_ano['Interactions Growth'] = media_interactions_ano.groupby('Account')['Total Interactions'].pct_change()
 
 import streamlit as st
 import pandas as pd
