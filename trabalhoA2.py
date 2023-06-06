29f1c2e27911bdd529658796b28cd5b8b7140859
gabimazzaffc <134002012+gabimazzaffc@users.noreply.github.com>
2023-06-06 14:07:45 -0300

    Update trabalhoA2.py

--- trabalhoA2.py
+++ trabalhoA2.py
@@ -1,6 +1,7 @@
 import streamlit as st
 import pandas as pd
 import seaborn as sns
+import matplotlib.pyplot as plt
 
 df = pd.read_parquet('dados_gruposoma.parquet')
 df['Post Created Date'] = pd.to_datetime(df['Post Created Date'])
@@ -13,6 +14,7 @@ def plot_interacoes_followers(df, username, ano):
     usuario = df[df['User Name'] == username].query('`Post Created Date` >= "' + ano + '-01-01" and `Post Created Date` <= "' + ano + '-12-31"')
     usuario_interactions = usuario.groupby('Post Created Date')['Total Interactions'].sum().reset_index()
     usuario_followers = usuario.groupby('Post Created Date')['Followers at Posting'].mean().reset_index()
+    fig, axes = plt.subplots(2,1, sharex=True, figsize=(18,10))
     fig.suptitle('Interações e Seguidores - ' + username + ' - ' + ano)
     sns.lineplot(ax=axes[0], data=usuario_interactions, x='Post Created Date', y='Total Interactions').set(title='Total de interações por dia - ' + username)
     sns.lineplot(ax=axes[1], data=usuario_followers, x='Post Created Date', y='Followers at Posting').set(title='Followers por dia - ' + username)
