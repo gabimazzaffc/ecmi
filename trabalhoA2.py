83e979b82e42a5a7b1ebc6bb3e533aef9421aa94
gabimazzaffc <134002012+gabimazzaffc@users.noreply.github.com>
2023-06-06 08:24:37 -0300

    Update trabalhoA2.py

--- trabalhoA2.py
+++ trabalhoA2.py
@@ -6,17 +6,23 @@ df['Post Created Date'] = pd.to_datetime(df['Post Created Date'])
 # df['Post Created'] = pd.to_datetime(df['Post Created'].str[:-4], format='%Y-%m-%d %H:%M:%S').dt.tz_localize('America/Sao_Paulo')
 df
 
-max_interactions = df.groupby('Account')['Total Interactions'].max()
-resultado = df[df['Total Interactions'].isin(max_interactions)]
-resultado
+def plot_interacoes_followers(df, username, ano):
+    if type(ano) != str:
+        ano = str(ano)
+    usuario = df[df['User Name'] == username].query('`Post Created Date` >= "' + ano + '-01-01" and `Post Created Date` <= "' + ano + '-12-31"')
+    usuario_interactions = usuario.groupby('Post Created Date')['Total Interactions'].sum().reset_index()
+    usuario_followers = usuario.groupby('Post Created Date')['Followers at Posting'].mean().reset_index()
+    fig, axes = plt.subplots(2, 1, sharex=True, figsize=(18, 10))
+    fig.suptitle('Interações e Seguidores - ' + username + ' - ' + ano)
+    sns.lineplot(ax=axes[0], data=usuario_interactions, x='Post Created Date', y='Total Interactions').set(title='Total de interações por dia - ' + username)
+    sns.lineplot(ax=axes[1], data=usuario_followers, x='Post Created Date', y='Followers at Posting').set(title='Followers por dia - ' + username)
+    plt.show()
+    
+plot_interacoes_followers(df, 'bynv', '2021')
 
 
-#df['Mês'] = df['Post Created Date'].dt.month
-#df['Ano'] = df['Post Created Date'].dt.year
-#media_interactions_mes_ano = df.groupby(['Account','Ano'])['Total Interactions'].mean().reset_index().sort_values(by=['Account', 'Ano'])
-#media_interactions_mes_ano['Interactions Growth'] = media_interactions_mes_ano.groupby('Account')['Total Interactions'].transform(lambda x: x.pct_change())
-#media_interactions_ano = df.groupby(['Account', 'Ano'])['Total Interactions'].mean().reset_index().sort_values(by=['Account', 'Ano'])
-#media_interactions_ano['Interactions Growth'] = media_interactions_ano.groupby('Account')['Total Interactions'].pct_change()
+
+
 
 #chart_data = df[[ 'Ano', 'Interactions Growth']]
 #st.line_chart(chart_data)
