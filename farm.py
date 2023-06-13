5c5aaa5788e89832e52c54823fa9096c4d0d1b0a
gabimazzaffc <134002012+gabimazzaffc@users.noreply.github.com>
2023-06-13 19:00:28 -0300

    Create farm.py

---
+++ farm.py
@@ -0,0 +1,12 @@
+import streamlit as st
+import pandas as pd
+import seaborn as sns
+import altair as alt
+
+st.header('Arquivo Contas do Instagram do Grupo Soma')
+df = pd.read_parquet('dados_farm.parquet')
+df['Post Created Date'] = pd.to_datetime(df['Post Created Date'])
+df['Total Interactions'] = df['Total Interactions'].apply(lambda x: int(x.replace(',', '')))
+st.dataframe(df)
+
+st.write(df[df['User Name'] == farmrio].sort_values(by='Total Interactions', ascending=False))
