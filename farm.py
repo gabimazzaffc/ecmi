1ad189b73f2a0a8afbc435170930f692150fe778
gabimazzaffc <134002012+gabimazzaffc@users.noreply.github.com>
2023-06-14 12:25:23 -0300

    Update farm.py

--- farm.py
+++ farm.py
@@ -9,10 +9,21 @@ df['Post Created Date'] = pd.to_datetime(df['Post Created Date'])
 df['Total Interactions'] = df['Total Interactions'].apply(lambda x: int(x.replace(',', '')))
 st.dataframe(df)
 
+
 st.header('Adoro farm')
 df_adorofarm = df[df['User Name']=="adorofarm"]
-df_adorofarm['Total Interactions'].max()
 st.write(df[df['User Name'] == 'adorofarm'].sort_values(by='Total Interactions', ascending=False))
+
+st.header('Farm rio')
+df_farmrio = df[df['User Name']=="farmrio"]
+st.write(df[df['User Name'] == 'farmrio'].sort_values(by='Total Interactions', ascending=False))
+
+with st.sidebar:
+    st.title('Titulo')
+    marca_sidebar = st.selectbox('Escolha uma marca para analisar:', ['bynv', 'adorofarm', 'foxtonbrasil', 'hering_oficial', 'animalebrasil', 'mariafilo'])
+    ano_sidebar = st.number_input('Escolha um ano', min_value=2019, max_value=2022, value=2021)
+
+
 
 def grafico_interativo_interacoes(df, username, ano):
     if type(ano) != str: 
@@ -23,9 +34,5 @@ def grafico_interativo_interacoes(df, username, ano):
     interacao = alt.Chart(usuario_interactions).mark_line().encode(x='Post Created Date', y='Total Interactions', tooltip=['Post Created Date', 'Total Interactions'])
     follower = alt.Chart(usuario_followers).mark_line().encode(x='Post Created Date', y='Followers at Posting', tooltip=['Post Created Date', 'Followers at Posting'])
     return interacao & follower
-st.altair_chart(grafico_interativo_interacoes(df, 'adorofarm', '2019'))
+st.altair_chart(grafico_interativo_interacoes(df, marca_sidebar, ano_sidebar), use_container_width=True)
 
-st.header('Farm rio')
-df_farmrio = df[df['User Name']=="farmrio"]
-df_farmrio['Total Interactions'].max() 
-st.write(df[df['User Name'] == 'farmrio'].sort_values(by='Total Interactions', ascending=False))
