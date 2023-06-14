b0b71b0a5b41c2ac02a2851209080548ad468d6a
gabimazzaffc <134002012+gabimazzaffc@users.noreply.github.com>
2023-06-14 10:13:39 -0300

    Update farm.py

--- farm.py
+++ farm.py
@@ -10,7 +10,31 @@ df['Total Interactions'] = df['Total Interactions'].apply(lambda x: int(x.replac
 st.dataframe(df)
 
 df_adorofarm = df[df['User Name']=="adorofarm"]
-df_farmrio = df[df['User Name']=="farmrio"]
-
-st.write(df[df['User Name'] == 'farmrio'].sort_values(by='Total Interactions', ascending=False))
+df_adorofarm['Total Interactions'].max()
 st.write(df[df['User Name'] == 'adorofarm'].sort_values(by='Total Interactions', ascending=False))
+df_adorofarm_mais2019 = df[df['User Name'] == 'adorofarm'].query('`Post Created Date` >= "2019-01-01" and `Post Created Date` <= "2023-05-01"')
+df_adorofarm_mais2019_interactions = df_adorofarm_mais2019.groupby('Post Created Date')['Total Interactions'].sum().reset_index()
+plt.figure(figsize=(12, 6))
+sns.lineplot(data=df_adorofarm_mais2019_interactions, x='Post Created Date', y='Total Interactions').set(title='Total de interações - adorofarm')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#df_farmrio = df[df['User Name']=="farmrio"]
+
+#st.write(df[df['User Name'] == 'farmrio'].sort_values(by='Total Interactions', ascending=False))
